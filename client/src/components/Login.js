43354370c81be22be0bc3b66067e4fbc85bc6a8b
Maderu-CM <clive.moyia@student.moringaschool.com>
2024-04-02 13:15:45 +0300

    Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,22 +4,38 @@ function Login() {
   return (
     <div>
       <h1>Login</h1>
+      <div
+        id="container"
+        className="d-flex justify-content-center align-items-center"
+      >
+        <form>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="email"
+              className="form-control form-control-sm"
+            ></input>
+          </div>
 
-      <form>
-        <div className="form-group">
-          <label>Email</label>
-          <input type="email" id="email" placeholder="email"></input>
-        </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="password"
+              className="form-control form-control-sm"
+            ></input>
+          </div>
 
-        <div className="form-group">
-          <label>Password</label>
-          <input type="password" id="password" placeholder="password"></input>
-        </div>
-
-        <div>
-        <button type="button" class="btn btn-success">Login</button>
-        </div>
-      </form>
+          <div className="form-group mt-3"> 
+            <button type="button" className="btn btn-success">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
