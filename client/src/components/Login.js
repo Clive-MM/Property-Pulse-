69cd24e7cf000f6be5e2b0ab2306e2e1992c513c
Maderu-CM <clive.moyia@student.moringaschool.com>
2024-04-02 12:02:57 +0300

    Login Page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,26 @@
-import React  from "react";
+import React from "react";
 
-function Login (){
-    return (
-        <h1>Login</h1>
-    );
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <form>
+        <div className="form-group">
+          <label>Email</label>
+          <input type="email" id="email" placeholder="email"></input>
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" id="password" placeholder="password"></input>
+        </div>
+
+        <div>
+        <button type="button" class="btn btn-success">Login</button>
+        </div>
+      </form>
+    </div>
+  );
 }
-export default Login
+export default Login;
